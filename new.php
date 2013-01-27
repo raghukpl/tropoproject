51cbb5cc644951eab04b59a4aa772b4c3515fb83
raghukpl <13may91@gmail.com>
2013-01-27 16:32:13 +0530

    de

--- new.php
+++ new.php
@@ -1,98 +1,44 @@
 <?php
-
-error_reporting(0);
-
-// include required class files.
-require_once 'classes/limonade.php';
-require_once 'classes/tropo.class.php';
-
-// Route for the initial caller dialog.
-dispatch_post('start', 'tropoStart');
-function tropoStart() {
-	
-	// Create a new instance of the Tropo object.
-	$tropo = new Tropo();	
-	
-	// Welcome message, and get caller input.
-	$tropo->say("Welcome to the tropo orchestra example.", array("barge" => false));
-	$tropo->ask("What is your favoirte programming language dude? You can say PHP, Ruby, Javascript or Python.", 
-				array("attempts" => 3,
-					  "choices" => "PHP, Ruby, JavaScript, Python", 
-					  "name" => "language", 
-					  "timeout" => 5)
-				);
-	
-	// Event handlers.
-	$tropo->on(array("event" => "continue", "next" => "index?uri=end", "say" => "Please hold."));
-	$tropo->on(array("event" => "error", "next" => "index?uri=error", "say" => "An error has occured."));
-	
-	// Render JSON for Tropo to consume.
-	$tropo->renderJSON();
-	
+ 
+require 'tropo.class.php';
+require 'lib/limonade.php';
+ 
+dispatch_post('start', 'app_start');
+function app_start() {
+     
+    $session = new Session();
+     
+    $tropo = new Tropo();
+     
+    if($session->getParameters("action")) {
+ 
+        $tropo->call("+14075550100", array('network'=>'SMS'));
+        $tropo->say("Received office voice mail.");
+         
+        $tropo->RenderJson();
+    }
+    else {
+         
+        $tropo->record(array("say"=>"We're not available right now, please leave your message at the beep.", "name" => "recording", "url" => "http://example.com/tropo"));
+         
+        $tropo->on(array("event" => "continue", "next" => "hello_world.php?uri=call"));
+        $tropo->on(array("event" => "hangup", "next" => "hello_world.php?uri=call"));
+         
+        $tropo->RenderJson();
+    }
 }
-
-// Route for the final caler dialog.
-dispatch_post('end', 'tropoEnd');
-function tropoEnd() {
-	
-	// Create a new instance of the result object, and get caller selection.
-	$result = new Result();
-	$selection = $result->getValue();	
-	
-	// Create a new instance of the Tropo object.
-	$tropo = new Tropo();
-	
-	// Alternate prompt based on caller selection.
-	switch($selection) {
-		
-		case 'PHP':
-			$toSay = "PHP is the shiz nit.";
-			break;
-			
-		case 'Ruby':
-			$toSay = "I have nothing against Ruby.";
-			break;
-			
-		case 'Javascript':
-			$toSay = "Javascript is mighty fine.";
-			break;
-			
-		case 'Python':
-			$toSay = "Python is for lovers.";
-		
-	}
-	
-	// Read out prompt based on caller langage selection.
-	$tropo->say("You chose, " . $selection);	
-	$tropo->say($toSay);	
-	$tropo->say("Thanks for playing along. Goodbye");	
-	
-	// Hangup when done.
-	$tropo->hangup();
-		
-	// Render JSON for Tropo to consume.
-	$tropo->renderJSON();
-
+ 
+dispatch_post('/call', 'app_call');
+function app_call() {
+    $token = 'your_token'; 
+    $curl_handle=curl_init(); 
+     
+    curl_setopt($curl_handle,CURLOPT_URL,'http://api.tropo.com/1.0/sessions?action=create&token='.$token); 
+    curl_setopt($curl_handle,CURLOPT_CONNECTTIMEOUT,2); 
+    curl_exec($curl_handle); 
+    curl_close($curl_handle); 
 }
-
-// Route for an error dialog.
-dispatch_post('error', 'tropoError');
-function tropoError() {
-	
-	// Create a new instance of the Tropo object.
-	$tropo = new Tropo();	
-	
-	// Error prompt.
-	$tropo->say("Sorry, an error has occured. Please try again later");
-
-	// Hangup when done.
-	$tropo->hangup();
-	
-	// Render JSON for Tropo to consume.
-	$tropo->renderJSON();
-	
-}
-
-// Run the app.
+ 
 run();
+ 
 ?>
